ed8dde413e535f200ab174575fc476beebeee6f2
melamit <leominer.work@gmail.com>
2024-06-15 04:13:07 +0300

    Revert "Initialize rework"
    This reverts commit a93a6be383223187397fa79a7906a65e9de37e9c.

--- src/main/kotlin/uno/redstone/redstoneclaims/commands/ChunkCommand.kt
+++ src/main/kotlin/uno/redstone/redstoneclaims/commands/ChunkCommand.kt
@@ -1,37 +1,35 @@
 package uno.redstone.redstoneclaims.commands
 
-import org.bukkit.command.CommandExecutor
 import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.command.TabCompleter
-
-// TODO: Check for permission before running a command
 
 class ChunkCommand : CommandExecutor, TabCompleter {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
         if (args == null) {
             return false
         }
-        return when (args.get(0)) {
+        return when (args[0]) {
             "claim" -> {
-                TODO()
+                ChunkClaim(sender)
+                true
             }
             "remove" -> {
-                TODO()
+                ChunkRemove(sender)
+                true
             }
-            "membership" -> {
-                TODO()
-            }
-
             else -> false
         }
-
     }
+
     override fun onTabComplete(sender: CommandSender, command: Command, label: String, args: Array<out String>?): MutableList<String>? {
-        return if (args == null) {
-            null
-        } else {
-            mutableListOf("claim", "remove", "ownership")
+        if (args == null) {
+            return null
+        }
+        return when (args.size) {
+            1 -> mutableListOf("claim", "remove")
+            else -> null
         }
     }
 }
